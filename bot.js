e3edb3274bb7e54e375b16ab2f9c8afe18ac12b1
XeroxYTB2 <hugocat40@gmail.com>
2026-01-17 01:11:02 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,25 +1,34 @@
 const mineflayer = require('mineflayer')
+const http = require('http')
+
+http.createServer((req, res) => {
+  res.end('Bot Minecraft actif')
+}).listen(3000)
 
 function startBot () {
+  console.log('🚀 Lancement du bot...')
+
   const bot = mineflayer.createBot({
     host: 'TheOnly.exaroton.me',
     port: 52424,
-    username: 'PierrePaul',
+    username: 'BotAFK',
     version: '1.21.1'
   })
 
   bot.once('spawn', () => {
     console.log('✅ Bot connecté')
-    setInterval(() => bot.swingArm('right'), 10000)
-  })
-
-  bot.on('end', () => {
-    console.log('🔄 Reconnexion...')
-    setTimeout(startBot, 5000)
+    setInterval(() => {
+      bot.swingArm('right')
+    }, 10000)
   })
 
   bot.on('kicked', r => console.log('❌ Kick:', r))
   bot.on('error', e => console.log('⚠️ Erreur:', e))
+
+  bot.on('end', () => {
+    console.log('🔄 Reconnexion dans 10s...')
+    setTimeout(startBot, 10000)
+  })
 }
 
 startBot()
